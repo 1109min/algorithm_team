36909072f9f48731cc859113afaf52a1100dae63
LeeJungKyun <naeric7@naver.com>
2022-11-14 17:18:34 +0900

    Firebase 연동 후 배열에 저장 HomeFragment.kt에 ingredientName ingredientAmount ingredientTime 저장 완료 전부 다 String

--- app/src/main/java/com/example/refrigerator/HomeFragment.kt
+++ app/src/main/java/com/example/refrigerator/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.example.refrigerator
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +21,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+    var arrayList: ArrayList<Any> = ArrayList()
+
+    //type 저장 변수
+    var ingredientName: ArrayList<String> = ArrayList()
+    var ingredientAmount: ArrayList<String> = ArrayList()
+    var ingredientTime: ArrayList<String> = ArrayList()
+
+    lateinit var ingredientTextView: TextView
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +47,40 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val binding = inflater.inflate(R.layout.fragment_home, container, false)
+        ingredientTextView = binding.findViewById(R.id.ingredient)
+        var firestore: FirebaseFirestore? = null
+        var uid: String? = null
+        uid = FirebaseAuth.getInstance().currentUser?.uid
+        firestore = FirebaseFirestore.getInstance();
+        firestore.collection("sourcefile").document("ingredients")
+            .addSnapshotListener { value, error ->
+                Log.d("event", value.toString())
+                arrayList.clear()
+                arrayList.addAll((value?.data?.get("ingredient") as ArrayList<*>))
+
+                //배열에 저장
+                for (i in 0 until arrayList.size) {
+                    try {
+                        if (i % 3 == 0)
+                            ingredientName.add(arrayList[i] as String)
+                        else if (i % 3 == 1)
+                            ingredientTime.add(arrayList[i].toString())
+                        if (i % 3 == 2)
+                            ingredientAmount.add(arrayList[i] as String)
+                    } catch (e: Exception) {
+                        Log.d("error:$i", e.toString())
+                    }
+                }
+
+                ingredientTextView.text = ingredientTime.toString()
+//                arrayList.add(35, "2000")
+//                firestore.collection("sourcefile").document("ingredients")
+//                    .update("ingredient", arrayList)
+
+                Log.d("event", arrayList.toString())
+            }
+        return binding
     }
 
     companion object {
--- app/src/main/java/com/example/refrigerator/MainActivity.kt
+++ app/src/main/java/com/example/refrigerator/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.refrigerator
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.refrigerator.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class MainActivity : AppCompatActivity() {
+
+
     private val viewBinding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,22 +27,22 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager
             .beginTransaction()
-            .replace(viewBinding.containerFragment.id,HomeFragment()) //add를 하면 겹친다
+            .replace(viewBinding.containerFragment.id, HomeFragment()) //add를 하면 겹친다
             .commitAllowingStateLoss() //그냥 commit하면 에러날 수도 있음
 
-        viewBinding.navBottom.run{
+        viewBinding.navBottom.run {
             setOnItemSelectedListener {
-                when(it.itemId){
+                when (it.itemId) {
                     R.id.menu_home -> {
                         supportFragmentManager
                             .beginTransaction()
-                            .replace(viewBinding.containerFragment.id,HomeFragment())
+                            .replace(viewBinding.containerFragment.id, HomeFragment())
                             .commitAllowingStateLoss()
                     }
                     R.id.menu_list -> {
                         supportFragmentManager
                             .beginTransaction()
-                            .replace(viewBinding.containerFragment.id,ListFragment())
+                            .replace(viewBinding.containerFragment.id, ListFragment())
                             .commitAllowingStateLoss()
                     }
                     R.id.menu_settings -> {
@@ -51,9 +57,15 @@ class MainActivity : AppCompatActivity() {
             selectedItemId = R.id.menu_home
         }
 
+
         //재료 추가 버튼
         viewBinding.mainBtn.setOnClickListener {
 
         }
+
+        //데이터베이스 연결
+
+
+
     }
 }
